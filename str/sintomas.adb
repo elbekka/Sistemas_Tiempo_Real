f6f8d4cb72cf8ed66fa499bcc9a04af442171a3f
elbekka <hakimelbekka@gmail.com>
2019-10-19 18:06:50 +0200

    eliminados _

--- str/sintomas.adb
+++ str/sintomas.adb
@@ -7,36 +7,36 @@ package body sintomas is
         procedure LeerCabeza(cabezaOut : out Boolean);
         Procedure EscribirCabeza(cabezaIn : in Boolean);
         private
-        _Cabeza : Boolean := False;
+        Cabeza : Boolean := False;
         end Cabeza;
     protected Distancia is
         Procedure LeerDistancia(distanciaOut:out Natural);
         Procedure EscribirDistancia(distanciaIn:in Natural);
         private
-        _Distancia : Natural range 1..3;
+        Distancia : Natural range 1..3;
         end Distancia;
 
     --Body Cabeza
     protected body Cabeza is 
         Procedure LeerCabeza(cabezaOut : out Boolean) is
         begin
-            cabezaOut:= _Cabeza;
+            cabezaOut:= Cabeza;
         end LeerCabeza;
         Procedure EscribirCabeza (cabezaIn : in Boolean) is 
         begin
-        _Cabeza:=cabezaIn;
+        Cabeza:=cabezaIn;
         end EscribirCabeza;
         end Cabeza;
 
     procedure body Distancia is 
         Procedure LeerDistancia(distanciaOut : out Natural) is 
         begin 
-            distanciaOut:=_Distancia;
+            distanciaOut:=Distancia;
         end LeerDistancia;
 
         procedure EscribirDistancia (distanciaIn : in Natural) is
         begin
-        _Distancia:=distanciaIn;
+        Distancia:=distanciaIn;
         end EscribirDistancia;
         end Distancia;
         begin 
