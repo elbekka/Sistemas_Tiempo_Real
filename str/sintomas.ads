24b4120ac8f4e30ee52e8a7c25cf3084cabe2e62
elbekka <hakimelbekka@gmail.com>
2019-10-19 18:25:48 +0200

    se han movido las declaraciones a .ads

--- str/sintomas.ads
+++ str/sintomas.ads
@@ -1,11 +1,13 @@
 package sintomas is 
- protected Cabeza is
+    protected Cabeza is
+        pragma priority (Ceiling_Priority_of_P);
         procedure LeerCabeza(cabezaOut : out Boolean);
         Procedure EscribirCabeza(cabezaIn : in Boolean);
         private
         CabezaVar : Boolean := False;
         end Cabeza;
     protected Distancia is
+        pragma priority (Ceiling_Priority_of_P);
         Procedure LeerDistancia(distanciaOut:out Natural);
         Procedure EscribirDistancia(distanciaIn:in Natural);
         private
