a3c6df6722de188c5520ac31ae4928342fc5c8c2
elbekka <hakimelbekka@gmail.com>
2019-10-19 18:27:45 +0200

    movido la declaracion de sintomas -> ads

--- str/sintomas.adb
+++ str/sintomas.adb
@@ -1,20 +1,6 @@
 
 package body sintomas is 
 
-    protected Cabeza is
-        pragma priority (Ceiling_Priority_of_P);
-        procedure LeerCabeza(cabezaOut : out Boolean);
-        Procedure EscribirCabeza(cabezaIn : in Boolean);
-        private
-        CabezaVar : Boolean := False;
-        end Cabeza;
-    protected Distancia is
-        pragma priority (Ceiling_Priority_of_P);
-        Procedure LeerDistancia(distanciaOut:out Natural);
-        Procedure EscribirDistancia(distanciaIn:in Natural);
-        private
-        DistanciaVar : Natural range 1..3;
-        end Distancia;
 
     --Body Cabeza
     protected body Cabeza is 
