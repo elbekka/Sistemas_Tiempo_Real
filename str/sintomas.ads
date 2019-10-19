40ec86fe2d94ee0a735d4366f28b4ac7c5392414
elbekka <hakimelbekka@gmail.com>
2019-10-19 18:11:55 +0200

    metido objeto protegido en el package de sintomas

--- str/sintomas.ads
+++ str/sintomas.ads
@@ -1,8 +1,15 @@
 package sintomas is 
-     Procedure LeerCabeza(cabezaOut : out Boolean);
-     Procedure EscribirCabeza (cabezaIn : in Boolean);
-
-     Procedure LeerDistancia(distanciaOut : out Natural);
-     Procedure EscribirDistancia (distanciaIn : in Natural);
+ protected Cabeza is
+        procedure LeerCabeza(cabezaOut : out Boolean);
+        Procedure EscribirCabeza(cabezaIn : in Boolean);
+        private
+        Cabeza : Boolean := False;
+        end Cabeza;
+    protected Distancia is
+        Procedure LeerDistancia(distanciaOut:out Natural);
+        Procedure EscribirDistancia(distanciaIn:in Natural);
+        private
+        Distancia : Natural range 1..3;
+        end Distancia;
 
 end sintomas;
