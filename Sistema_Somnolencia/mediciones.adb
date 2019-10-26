a624965a0e68f3aa5fee5e5a246edc5d33b03f6a
elbekka <hakimelbekka@gmail.com>
2019-10-26 11:34:43 +0200

    se ha añadido el body en la implementacion

--- Sistema_Somnolencia/mediciones.adb
+++ Sistema_Somnolencia/mediciones.adb
@@ -1,18 +1,20 @@
 package body mediciones is  
   --body de las mediciones
-  protected Protected_Mediciones is 
+  protected body Protected_Mediciones is 
     procedure LeerDistancia(DistanciaOut : out Distance_Samples_Type) is 
       begin
         DistanciaOut := distancia;
       end LeerDistancia;
-    Procedure EscribirDistancia(DistanciaIn : in Distance_Samples_Type) is
+    Procedure body EscribirDistancia(DistanciaIn : in Distance_Samples_Type) is
       begin
         distancia:=DistanciaIn;
       end EscribirDistancia;
-    procedure LeerVelocidad(VelocidadOut : out Speed_Samples_Type) is
+    procedure body LeerVelocidad(VelocidadOut : out Speed_Samples_Type) is
+      begin
       VelocidadOut:= velocidad;
       end LeerVelocidad;
-    procedure EscribirVelocidad(VelocidadIn : in Speed_Samples_Type) is
+    procedure body  EscribirVelocidad(VelocidadIn : in Speed_Samples_Type) is
+      begin
       velocidad:=VelocidadIn;
      end EscribirVelocidad;
     end Protected_Mediciones;
